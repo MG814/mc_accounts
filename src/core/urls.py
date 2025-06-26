27114f6a0bc4d20558adb6ec22169798a0da59cf
MICHAŁ <mg814@wp.pl>
2025-06-26 22:14:27 +0200

    refactor: remove not using imports

--- src/core/urls.py
+++ src/core/urls.py
@@ -1,12 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
-from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView, SpectacularRedocView
+from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('accounts.urls')),  # api/v1/
+    path('', include('accounts.urls')),
     path('address/', include('address.urls')),
     path('schema/', SpectacularAPIView.as_view(), name='schema'),
-    # path('schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
     path('schema/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
 ]
